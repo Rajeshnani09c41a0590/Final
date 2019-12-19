5802dc166583fbbcd691cc3034d9f8b3b82cb611
Rajeshnani09c41a0590 <33112634+Rajeshnani09c41a0590@users.noreply.github.com>
2019-12-19 19:34:46 +0530

    Delete display.pipe.spec.ts

--- webapp/src/app/pipe/display.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DisplayPipe } from './display.pipe';
-
-describe('DisplayPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DisplayPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
